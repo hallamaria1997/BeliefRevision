7df0d4ac07ca4c4e0e6457c6b8f76f1ff3f09bec
hallamh16 <50322630+hallamh16@users.noreply.github.com>
2022-04-20 17:41:37 +0200

    '..'

--- beliefbase.py
+++ beliefbase.py
@@ -1,31 +1,21 @@
-<<<<<<< HEAD
 import re
 from sympy.logic.boolalg import to_cnf
-
-class BeliefBase:
-    cnf_format: str
-    return_belief: str
-
-    def __init__(self, name=None, board=None):
-        #TODO check if needs to be some other structure
-        self.beliefs = []
-        self.cnf = []
-        print("calling belief base ")
-
-    def add(self, belief):
-        """Adds belief from user input to the base"""
-        #add verification step
-        if '<>' in belief:
-            belief = self.parsing_bicond(belief)
-        self.cnf.append(to_cnf(belief))
-        self.beliefs.append(belief)
-=======
 import queue
 from belief import Belief
+
+#def add(self, belief):
+#        """Adds belief from user input to the base"""
+#        #add verification step
+#        if '<>' in belief:
+#            belief = self.parsing_bicond(belief)
+#        self.cnf.append(to_cnf(belief))
+#        self.beliefs.append(belief)
+
 
 class BeliefBase:
     beliefBase : dict
     beliefCount : int
+    return_belief: str
 
     def __init__(self):
         self.beliefBase = {}
@@ -56,7 +46,6 @@ class BeliefBase:
             self.expand(belief)
 
         return 1
->>>>>>> 7cd216a62ea6ec5e4431b53e081b486b2d38bbb6
 
     
 
@@ -73,19 +62,11 @@ class BeliefBase:
 
 
     def clear(self):
-<<<<<<< HEAD
         """Clears all beliefs from the BeliefBase"""
-        self.beliefs = []
-
-    def get(self):
-        """Returns all beliefs in the base"""
-        return self.beliefs
-=======
-        self.beliefBase = {}
+        self.beliefs = {}
 
     def get(self):
         return list(self.beliefBase.values())
->>>>>>> 7cd216a62ea6ec5e4431b53e081b486b2d38bbb6
 
     def validate_formatting(self, belief):
         return True
